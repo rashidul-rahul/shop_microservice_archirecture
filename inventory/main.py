49b64919fdbfd7b66e069c9799417865b06bf1f0
Rashidul Rahul <rashidul.rahul@goama.com>
2023-03-08 19:03:18 +0600

    implement all API for inventory service

--- inventory/main.py
+++ inventory/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 
 
 app = FastAPI()
 
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],
+        allow_methods=['*'],
+        allow_headers=['*']
+        )
+
 redis = get_redis_connection(
-        host="",
-        port="",
-        password="",
+        host="redis-19644.c1.ap-southeast-1-1.ec2.cloud.redislabs.com",
+        port="19644",
+        password="E6fzwFJ9sTjXZUGVBSr9uOuLFWztyk6m",
         decode_responses=True
         )
 
@@ -21,6 +29,32 @@ class Product(HashModel):
         database = redis
 
 
-@app.get('/products')
+def formant_product(pk: str):
+    product = Product.get(pk)
+
+    return {
+            "id": product.pk,
+            "name": product.name,
+            "price": product.price,
+            "quantity": product.quantity
+            }
+
+
+@app.get('/all/products')
 def get_products():
-    return []
+    return [formant_product(pk) for pk in Product.all_pks()]
+
+
+@app.post("/product")
+def create_product(product: Product):
+    return product.save()
+
+
+@app.get("/product/{pk}")
+def deg_product(pk: str):
+    return Product.get(pk)
+
+
+@app.delete("/product/{pk}")
+def delete_product(pk: str):
+    return Product.delete(pk)
